b1d9fa85aea724e73c5d96eec51a5b8f3e129938
Alex-GF <yalejandro9@gmail.com>
2025-07-03 13:53:43 +0200

    hotfix: tests

--- src/test/java/io/github/isagroup/PricingEvaluatorUtilTests.java
+++ src/test/java/io/github/isagroup/PricingEvaluatorUtilTests.java
@@ -1,6 +1,7 @@
 package io.github.isagroup;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +19,7 @@ public class PricingEvaluatorUtilTests {
     private static final String JWT_EXPRESSION_TEST = "userContext['pets']*4 < planContext['usageLimits']['pets']";
 
     private static final String USER_PLAN = "ADVANCED";
+    private static final List<String> USER_ADDONS = List.of("addOnFeature", "extraPets");
     private static final String YAML_CONFIG_PATH = "pricing/petclinic.yml";
 
     private PricingContext pricingContext;
@@ -39,6 +41,7 @@ public class PricingEvaluatorUtilTests {
         pricingContext.setJwtSecret(JWT_SECRET_TEST);
         pricingContext.setUserContext(userContext);
         pricingContext.setUserPlan(USER_PLAN);
+        pricingContext.setUserAddOns(USER_ADDONS);
         pricingContext.setConfigFilePath(YAML_CONFIG_PATH);
 
         this.pricingContext = pricingContext;
